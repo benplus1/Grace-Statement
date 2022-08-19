14a882db002566cf1c4de7818fd4ef796d92c801
Benjamin   Yang <by2297@hulk.cs.columbia.edu>
2022-08-19 18:50:09 -0400

    a

--- Code/Default/pkl_test.py
+++ Code/Default/pkl_test.py
@@ -4,11 +4,13 @@ import json
 import sys
 import os
 
-with open('results/Chart/Chartres_0_0.01_60.pkl', 'rb') as f:
+#results/Chart/Chartres0_0_0.01_60.pkl
+
+with open('dataset/Time.pkl', 'rb') as f:
     data = pickle.load(f)
 
 
 json_obj = json.loads(json.dumps(data, default=str))
 
-with open('results/Chart/Chartres_0_0.01_60.json', 'w', encoding='utf-8') as outfile:
+with open('dataset/Time.json', 'w', encoding='utf-8') as outfile:
     json.dump(json_obj, outfile, ensure_ascii=False, indent=4)
---
+++ Code/Default/prepare_for_analysis.py
@@ -0,0 +1,32 @@
+# import libraries
+import pickle
+import json
+import sys
+import os
+
+project_name = "Chart"
+
+with open('dataset/jsonConversions/' + project_name + '.json', 'rb') as f:
+    project_json = json.load(f)
+
+
+with open('results/' + project_name + '/' + project_name + 'res_0_0.01_60.pkl', 'rb') as g:
+    data = pickle.load(g)
+
+results_json = json.loads(json.dumps(data, default=str))
+
+# print(len(results_json))
+# print(len(project_json))
+assert len(results_json) == len(project_json)
+
+write_data = {}
+
+for i, proj_data in enumerate(project_json):
+    bug_name = proj_data['proj']
+    bug_lans = proj_data['lans']
+    best_epoch_results = results_json[str(i)][1]
+    write_data[bug_name] = {"lans" : bug_lans, "labels" : best_epoch_results}
+
+with open('outputs/' + project_name + '_results.json', 'w', encoding='utf-8') as outfile:
+    write_json = json.loads(json.dumps(write_data, default=str))
+    json.dump(write_json, outfile, ensure_ascii=False, indent=4)
